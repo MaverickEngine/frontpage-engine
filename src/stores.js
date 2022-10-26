5b4de4a42f2014194c7ee7e752951cc2f76d8fbd
Jason Norwood-Young <jason@10layer.com>
2022-10-26 14:35:17 +0200

    featuredPosts, unfeaturedPosts, unorderedPosts

--- src/stores.js
+++ src/stores.js
@@ -1,28 +1,5 @@
 import { writable } from 'svelte/store';
 
-export const featuredPosts = writable([
-    {
-        id: 1,
-        title: "Hello world",
-        author: "John Doe",
-        excerpt: "This is an excerpt",
-        thumbnail: "https://via.placeholder.com/150",
-        link: "https://example.com"
-    },
-    {
-        id: 2,
-        title: "Hello world 2",
-        author: "Jane Doe",
-        excerpt: "This is an excerpt 2",
-        thumbnail: "https://via.placeholder.com/150",
-        link: "https://example.com"
-    },
-    {
-        id: 3,
-        title: "Hello world 3",
-        author: "John Donne",
-        excerpt: "This is an excerpt 3",
-        thumbnail: "https://via.placeholder.com/150",
-        link: "https://example.com"
-    },
-]);
+export const featuredPosts = writable([]);
+export const unfeaturedPosts = writable([]);
+export const unorderedPosts = writable([]);
