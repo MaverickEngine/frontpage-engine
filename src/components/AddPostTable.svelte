529d07bb2d20581f619d41a35d7ce19f30ca4544
Jason Norwood-Young <jason@10layer.com>
2023-04-14 09:17:47 +0200

    Search

--- src/components/AddPostTable.svelte
+++ src/components/AddPostTable.svelte
@@ -1,115 +1,122 @@
 <script>
     const mode = process.env.NODE_ENV;
     import { featuredPosts, unfeaturedPosts, unorderedPosts } from '../stores.js';
+    import Search from "./Search.svelte";
     import { fly } from 'svelte/transition';
     import { onMount } from 'svelte';
     import PostRow from "./PostRow.svelte";
-    import { wp_api_post } from "../lib/wp_api.js";
     import { apiGet, apiPost } from "../lib/ajax.ts";
-    import { map_posts, applyLockedSlots } from "../lib/posts.js";
+    import { map_posts } from "../lib/posts.js";
 
     import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
     
     export let frontpage_id;
-    export let type = "unfeatured"; // "unfeatured" or "unordered"
-    let posts = [];
 
-    // const dragStart = (e, i) => {
-    //     console.log("dragStart", i);
-    //     e.dataTransfer.effectAllowed = 'move';
-    //     e.dataTransfer.dropEffect = 'move';
-    //     const start = i;
-    //     e.dataTransfer.setData('text/plain', start);
-    //     dispatch("dragstart");
-    // }
+    let posts = [];
+    let analytics = [];
+    let total_hits = 0;
+    let search = "";
+    let page = 1;
+    let per_page = 10;
 
     const featurePost = async (post, position) => {
         $featuredPosts = (await apiPost(`frontpageengine/v1/add_post/${frontpage_id}`, {
             post_id: post.id,
             position,
-        }, "featuredPosts")).posts.map(map_posts);
+        })).posts.map(map_posts);
         posts = posts.filter(p => p.id !== post.id);
         dispatch("updated");
     }
 
-    onMount(async () => {
-        if (type==="unfeatured") {
-            posts = $unfeaturedPosts;
-            const result = await apiGet(`frontpageengine/v1/unfeatured_posts/${frontpage_id}`);
-            $unfeaturedPosts = result.posts.map(map_posts);
-            posts = $unfeaturedPosts;
-            // console.log(posts);
-        } else if (type==="unordered") {
-            posts = $unorderedPosts;
-            const result = await wp_api_post("frontpage_engine_fetch_unordered_posts", {
-                id: frontpage_id,
-            });
-            $unorderedPosts = result.map(map_posts);
-            posts = $unorderedPosts;
+    const getPosts = async() => {
+        const result = await apiGet(`frontpageengine/v1/unfeatured_posts/${frontpage_id}?search=${search}&page=${page}&per_page=${per_page}`);
+        $unfeaturedPosts = result.posts.map(map_posts);
+        posts = $unfeaturedPosts;
+        getAnalytics();
+    }
+
+    const getAnalytics = async () => {
+        const post_ids = posts.map(p => p.id);
+        console.log("post_ids", post_ids);
+        if ((mode === "development")) {
+            analytics = Object.values((await apiPost(`frontpageengine/v1/analytics?simulate_analytics=1`, {post_ids})).analytics);
+        } else {
+            analytics = Object.values((await apiPost(`frontpageengine/v1/analytics`, {post_ids})).analytics);
         }
+        total_hits = analytics.reduce((a, b) => a + b.hits_last_hour, 0);
+        console.log("totalHits", total_hits);
+    }
+
+    onMount(async () => {
+        getPosts();
     });
 </script>
 
-<table class="wp-list-table widefat fixed striped table-view-list featuredposts">
-    <thead>
-        <tr>
-            <td class="manage-column check-column">
-                <label class="screen-reader-text" for="cb-select-all-1">Select All</label>
-                <input class="cb-select-all-1" type="checkbox" />
-            </td>
-            <th scope="col" class="column-header-image">Image</th>
-            {#if (mode === "development")}
-            <th scope="col" class="manage-column">Slot ID</th>
-            <th scope="col" class="manage-column">Post ID</th>
-            <th scope="col" class="manage-column">Display Order</th>
-            {/if}
-            <th scope="col" class="column-header-title">Title</th>
-            <th scope="col" class="manage-column">Author</th>
-            <th scope="col" class="manage-column">Published</th>
-            <th></th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each posts as post, index (post.id)}
-        <tr 
-            id="post-{post.id}"
-            out:fly|local="{{ y: -200, duration: 600 }}"
-        >
-            <th scope="row" class="check-column">
-                <label class="screen-reader-text" for="cb-select-1">Select</label>
-                <input class="cb-select-1" type="checkbox" />
-            </th>
-            <PostRow 
-                post={post} 
-                index={index}
-            />
-            <th>
-                <button 
-                    class="button button-primary"
-                    on:click="{featurePost(post, "top")}"
-                >
-                    Top
-                </button>
-                <button 
-                    class="button button-primary"
-                    on:click="{featurePost(post, "bottom")}"
-                >
-                    Bottom
-                </button>
-                <button 
-                    class="button button-primary"
-                    on:click="{featurePost(post, "auto")}"
-                >
-                    Auto
-                </button>
-            </th>
-        </tr>
-        {/each}
-        
-    </tbody>
-</table>
+<div class="frontpageengine-addpost-container">
+    <div class="frontpageengine-addpost-search">
+        <Search bind:value={search} on:search={getPosts} />
+    </div>
+    <table class="wp-list-table widefat fixed striped table-view-list featuredposts">
+        <thead>
+            <tr>
+                <td class="manage-column check-column">
+                    <label class="screen-reader-text" for="cb-select-all-1">Select All</label>
+                    <input class="cb-select-all-1" type="checkbox" />
+                </td>
+                <th scope="col" class="column-header-image">Image</th>
+                {#if (mode === "development")}
+                <th scope="col" class="manage-column">Slot ID</th>
+                <th scope="col" class="manage-column">Post ID</th>
+                <th scope="col" class="manage-column">Display Order</th>
+                {/if}
+                <th scope="col" class="column-header-title">Title</th>
+                <th scope="col" class="manage-column">Author</th>
+                <th scope="col" class="manage-column">Published</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each posts as post, index (post.id)}
+            <tr 
+                id="post-{post.id}"
+                out:fly|local="{{ y: -200, duration: 600 }}"
+            >
+                <th scope="row" class="check-column">
+                    <label class="screen-reader-text" for="cb-select-1">Select</label>
+                    <input class="cb-select-1" type="checkbox" />
+                </th>
+                <PostRow 
+                    post={post} 
+                    index={index}
+                />
+                <th>
+                    <button 
+                        class="button button-primary"
+                        on:click="{featurePost(post, "top")}"
+                    >
+                        Top
+                    </button>
+                    <button 
+                        class="button button-primary"
+                        on:click="{featurePost(post, "bottom")}"
+                    >
+                        Bottom
+                    </button>
+                    <button 
+                        class="button button-primary"
+                        on:click="{featurePost(post, "auto")}"
+                    >
+                        Auto
+                    </button>
+                </th>
+            </tr>
+            {/each}
+            
+        </tbody>
+    </table>
+</div>
 
 <style>
     tr.is-active {
@@ -122,5 +129,9 @@
 
     .column-header-title {
         width: 500px;
+    }
+
+    .frontpageengine-addpost-search {
+        margin-bottom: 20px;
     }
 </style>
---
+++ src/components/Search.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+    export let value = "";
+    export let debounce = 500;
+    let timer;
+
+    const doSearch = () => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            dispatch("search", value);
+        }, debounce);
+    }
+</script>
+
+<input 
+    type="text" 
+    placeholder="Search"
+    bind:value={value}
+    on:keyup={doSearch}
+/>
